745a42e802fe70ea666e94e8d3873cbdd48d104f
kevinmcentee <kmcentee@gmail.com>
2022-11-12 09:57:12 -0800

    Initial version

---
+++ cliWildFireDetect.py
@@ -0,0 +1,36 @@
+import sys
+
+from pyrovision.models import rexnet1_0x
+from torchvision import transforms
+import torch
+from PIL import Image
+
+
+# Init
+print("Setting up transform...")
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+# original transform from https://github.com/pyronear/pyro-vision/blob/master/README.md
+# as of date November 11, 2022
+#theTransform = transforms.Compose([transforms.Resize(size=448), transforms.CenterCrop(size=448),
+#                                  transforms.ToTensor(), normalize])
+
+# new transform height and width from Mateo email of November 12, 2022
+theTransform = transforms.Compose([transforms.Resize([256, 384]),
+                                  transforms.ToTensor(), normalize])
+
+print("setting up model...")
+model = rexnet1_0x(pretrained=True).eval()
+
+# Predict
+numberOfFiles = len(sys.argv)
+for index in range(1, numberOfFiles):
+    theImage = theTransform(Image.open(sys.argv[index]).convert('RGB'))
+
+    with torch.no_grad():
+        prediction = model(theImage.unsqueeze(0))
+        is_wildfire = torch.sigmoid(prediction).item() >= 0.5
+
+    print("Predicting", sys.argv[index], is_wildfire)
+
+print("Finished")
